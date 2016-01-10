1f4eaddff3fe7cb90b985eaab05e544091010f38
bmldas132 <bimal@pixelstroke.com>
2016-01-10 20:19:45 +0530

    update

---
+++ AuthenticationUsingWebApi/Models/CustomClass/Users.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AuthenticationUsingWebApi.Models.CustomClass
+{
+    public class Users
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public bool EmailConfirmed { get; set; }
+        public string PhoneNumber { get; set; }
+        public bool PhoneNumberConfirmed { get; set; }
+        public string UserName { get; set; }
+    }
+}
